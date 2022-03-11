bfac04d565db62849b63804c24d6492fb4376eca
chippedfurniture <79117873+chippedfurniture@users.noreply.github.com>
2022-03-11 14:31:05 -0800

    Add files via upload

--- Scripts/Obstacles.gd
+++ Scripts/Obstacles.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+onready var obstacle_sprite = $AnimatedSprite
+
 
 const TARGET_Y = 155
 const SPAWN_Y = -16
@@ -9,12 +11,12 @@ const LEFT_LANE_SPAWN = Vector2(115, SPAWN_Y)
 const CENTRE_LANE_SPAWN = Vector2(165, SPAWN_Y)
 const RIGHT_LANE_SPAWN = Vector2(205, SPAWN_Y)
 
-var speed = 0
+export var speed = 0
 var hit = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	obstacle_sprite.frame = floor(rand_range(0,2.99))
 
 func _physics_process(delta):
 	if !hit:
@@ -24,22 +26,25 @@ func _physics_process(delta):
 			#get_parent().reset_combo()
 	else:
 		$Node2D.position.y -= speed * delta
+	
+	
 
 func initialize(lane):
-	if lane == 0:
-		$AnimatedSprite.frame = 0
-		position = LEFT_LANE_SPAWN
-	elif lane == 1:
-		$AnimatedSprite.frame = 1
-		position = CENTRE_LANE_SPAWN
-	elif lane == 2:
-		$AnimatedSprite.frame = 2
-		position = RIGHT_LANE_SPAWN
-	else:
-		printerr("Invalid lane set for note: " + str(lane))
-		return
+	pass
+	# if lane == 0:
+	#	$AnimatedSprite.frame = 0
+	#	position = LEFT_LANE_SPAWN
+	#elif lane == 1:
+	#	$AnimatedSprite.frame = 1
+	#	position = CENTRE_LANE_SPAWN
+	#elif lane == 2:
+	#	$AnimatedSprite.frame = 2
+	#	position = RIGHT_LANE_SPAWN
+	#else:
+	#	printerr("Invalid lane set for note: " + str(lane))
+	#	return
 	
-	speed = DIST_TO_TARGET / 2.0
+	
 
 func destroy():
 	$CPUParticles2D.emitting = true
